18243cb6f754eac102e33c875a0ce1cdea46a351
hariomkantsharma <hariomkantsharma@gmail.com>
2024-10-24 22:22:36 +0530

    implemented singleton pattern for database

--- app/src/main/java/com/haridroid/dayremarker/MyDBhelper.java
+++ app/src/main/java/com/haridroid/dayremarker/MyDBhelper.java
@@ -17,6 +17,9 @@ public class MyDBhelper extends SQLiteOpenHelper {
     private static final int DATABASE_VERSION=1;
     private static final String TABLE_NAME= "2024";
 
+    private static MyDBhelper instance;
+
+
     private static final String COL_1="ID";
     private static final String COL_2="MONTH";
     private static final String COL_3="DAY";
@@ -27,6 +30,14 @@ public class MyDBhelper extends SQLiteOpenHelper {
     public MyDBhelper(@Nullable Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
+
+    public static synchronized MyDBhelper getInstance(Context context){
+        if(instance==null){
+            instance= new MyDBhelper(context.getApplicationContext());
+        }
+        return instance;
+    }
+
 
     @Override
     public void onCreate(SQLiteDatabase db) {
